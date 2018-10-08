8d0a1dba7d7992cc95211d8bdb96c188409a75b8
Matt Murphy <33004770+MattMurphyDev@users.noreply.github.com>
2018-10-08 19:37:49 -0400

    Add d6 button
    Added a button with an onclick that calls the rolling function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 var dieRoll = 0;
 
-function rollDie() {
+function rollD6() {
   dieRoll = Math.floor(Math.random() * 6 +1);
+  alert(dieRoll);
 }
 
-for (var i = 0; i < 5; i += 1) {
+/* for (var i = 0; i < 5; i += 1) {
   rollDie();
   document.write(dieRoll);
-}
+} */
